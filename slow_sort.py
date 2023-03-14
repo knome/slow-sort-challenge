58b7ef740948d0b99aa90069c5f502fb19d7af7e
Michael Speer <knomenet@gmail.com>
2023-03-14 00:19:34 -0400

    slow

--- slow_sort.py
+++ slow_sort.py
@@ -1,13 +1,41 @@
 import time
 
+g_picks     = 0
+g_rotations = 0
+g_swaps     = 0
+
+def swap_adjacent( numbers, at ):
+    numbers[at    ] ^= numbers[at + 1]
+    numbers[at + 1] ^= numbers[at    ]
+    numbers[at    ] ^= numbers[at + 1]
+    global g_swaps
+    g_swaps += 1
+
+def rotate_segment( numbers, start ):
+    for ii in range( len( numbers[ start : ] ) - 1 ):
+        swap_adjacent( numbers, start + ii )
+    global g_rotations
+    g_rotations += 1
+
 def slow_sort(numbers: list) -> None:
-    # your code here to sort the numbers
-    pass
+    at = 0
+    while at < len( numbers ):
+        nn = len( numbers ) - at - 1
+        ii = 0
+        while ii < nn:
+            if numbers[at] > numbers[at + 1]:
+                global g_picks
+                g_picks += 1
+                swap_adjacent( numbers, at )
+            rotate_segment( numbers, at + 1 )
+            ii += 1
+        at += 1
 
 def main() -> None:
     # Uncomment the following line to read from a file.
-    # input_file = open('numbers50.txt', 'r')
-    # input = input_file.readline
+    
+    input_file = open('numbers1000.txt', 'r')
+    input = input_file.readline
     
     size = int(input())
     numbers = [0] * size
@@ -16,7 +44,7 @@ def main() -> None:
         numbers[i] = int(input())
     
     slow_sort(numbers)
-
+    
     with open(f'output{size}.txt', 'w') as file:
         for number in numbers:
             file.write(f'{number}\n')
@@ -27,3 +55,4 @@ if __name__ == '__main__':
     end_time = time.time_ns() / 1000000
     
     print(f'Elapsed time: {end_time - start_time} milliseconds.')
+    print(f'gPicks={g_picks} gRotations={g_rotations} gSwaps={g_swaps}' )
